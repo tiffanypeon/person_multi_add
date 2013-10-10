9fdad5a24c159c84d67455af45e7a81da23c0700
tiffanypeon <tiffanypeon@gmail.com>
2013-10-10 09:30:43 -0400

    solution to todo

---
+++ person.rb
@@ -0,0 +1,13 @@
+class Person 
+
+  attr_accessor :attributes, :name, :birthday, :hair_color, :eye_color, 
+  :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size,
+  :glove_size, :pant_length, :pant_width
+
+  def initialize(hash)
+    hash.map do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+
+end
